9abe14cac3f28658670de93bd02392b82e9a44d5
zenverak <zenverak@gmail.com>
2014-04-09 14:35:23 -0400

    Updated DCM bot for HTML and some in JS
    I've mostly been working on the HTML version of DCMBot for Javascript. I
    have decided to move the website to weebly with another website since
    Google sites seems to do some odd things to Javascript code.

--- DCMBotJavaScript/CMDBot.js
+++ DCMBotJavaScript/CMDBot.js
@@ -29,149 +29,173 @@ function dcm() {
     }
 
     function lineup() {
-        var team = {
-            1: {
-                name: "Freddie Freeman",
-                used: false,
-                onTeam: true
-            },
-            2: {
-                name: "BJ Upton",
-                used: false,
-                onTeam: true
-            },
-            3: {
-                name: "Justin Upton",
-                used: false,
-                onTeam: true
-            },
-            4: {
-                name: "Evan Gattis",
-                used: false,
-                onTeam: true
-            },
-            5: {
-                name: "Chris Johnson",
-                used: false,
-                onTeam: true
-            },
-            6: {
-                name: "Dan Uggla",
-                used: false,
-                onTeam: true
-            },
-            7: {
-                name: "Alex Wood",
-                used: false,
-                onTeam: true
-            },
-            8: {
-                name: "David Hale",
-                used: false,
-                onTeam: true
-            },
-            9: {
-                name: "Luis Avilan",
-                used: false,
-                onTeam: true
-            },
-            10: {
-                name: "Pedro Beato",
-                used: false,
-                onTeam: true
-            },
-            11: {
-                name: "David Carpenter",
-                used: false,
-                onTeam: true
-            },
-            12: {
-                name: "Aaron Harang",
-                used: false,
-                onTeam: true
-            },
-            13: {
-                name: "Gus Schlosser",
-                used: false,
-                onTeam: true
-            },
-
-            14: {
-                name: "Julio Teheran",
-                used: false,
-                onTeam: true
-            },
-            15: {
-                name: "Ian Thomas",
-                used: false,
-                onTeam: true
-            },
-            16: {
-                name: "Anthony Varvaro",
-                used: false,
-                onTeam: true
-            },
-            17: {
-                name: "Ryan Doumit",
-                used: false,
-                onTeam: true
-            },
-            18: {
-                name: "Gerald Laird",
-                used: false,
-                onTeam: true
-            },
-            19: {
-                name: "Tyler Pastornicky",
-                used: false,
-                onTeam: true
-            },
-            20: {
-                name: "Ramiro Pena",
-                used: false,
-                onTeam: true
-            },
-            21: {
-                name: "Andrelton Simmons",
-                used: false,
-                onTeam: true
-            },
-            22: {
-                name: "Jason Heyward",
-                used: false,
-                onTeam: true
-            },
-            23: {
-                name: "Jordan Schafer",
-                used: false,
-                onTeam: true
-            }
-        },
+                var team = {
+                    1: {
+                        name: "Freddie Freeman",
+                        used: false,
+                        onTeam: true,
+                        type: "h"
+                    },
+                    2: {
+                        name: "BJ Upton",
+                        used: false,
+                        onTeam: true,
+                        type: "h"
+                    },
+                    3: {
+                        name: "Justin Upton",
+                        used: false,
+                        onTeam: true,
+                        type: "h"
+                    },
+                    4: {
+                        name: "Evan Gattis",
+                        used: false,
+                        onTeam: true,
+                        type: "h"
+                    },
+                    5: {
+                        name: "Chris Johnson",
+                        used: false,
+                        onTeam: true,
+                        type: "h"
+                    },
+                    6: {
+                        name: "Dan Uggla",
+                        used: false,
+                        onTeam: true,
+                        type: "h"
+                    },
+                    7: {
+                        name: "Alex Wood",
+                        used: false,
+                        onTeam: true,
+                        type: "p"
+                    },
+                    8: {
+                        name: "David Hale",
+                        used: false,
+                        onTeam: true,
+                        type: "p"
+                    },
+                    9: {
+                        name: "Luis Avilan",
+                        used: false,
+                        onTeam: true,
+                        type: "p"
+                    },
+                    10: {
+                        name: "Pedro Beato",
+                        used: false,
+                        onTeam: true,
+                        type: "p"
+                    },
+                    11: {
+                        name: "David Carpenter",
+                        used: false,
+                        onTeam: true,
+                        type: "p"
+                    },
+                    12: {
+                        name: "Aaron Harang",
+                        used: false,
+                        onTeam: true,
+                        type: "p"
+                    },
+                    13: {
+                        name: "Gus Schlosser",
+                        used: false,
+                        onTeam: true,
+                        type: "p"
+                    },
+                    14: {
+                        name: "Julio Teheran",
+                        used: false,
+                        onTeam: true,
+                        type: "p"
+                    },
+                    15: {
+                        name: "Ian Thomas",
+                        used: false,
+                        onTeam: true,
+                        type: "p"
+                    },
+                    16: {
+                        name: "Anthony Varvaro",
+                        used: false,
+                        onTeam: true,
+                        type: "p"
+                    },
+                    17: {
+                        name: "Ryan Doumit",
+                        used: false,
+                        onTeam: true,
+                        type: "h"
+                    },
+                    18: {
+                        name: "Gerald Laird",
+                        used: false,
+                        onTeam: true,
+                        type: "h"
+                    },
+                    19: {
+                        name: "Tyler Pastornicky",
+                        used: false,
+                        onTeam: true,
+                        type: "h"
+                    },
+                    20: {
+                        name: "Ramiro Pena",
+                        used: false,
+                        onTeam: true,
+                        type: "h"
+                    },
+                    21: {
+                        name: "Andrelton Simmons",
+                        used: false,
+                        onTeam: true,
+                        type: "h"
+                    },
+                    22: {
+                        name: "Jason Heyward",
+                        used: false,
+                        onTeam: true,
+                        type: "h"
+                    },
+                    23: {
+                        name: "Jordan Schafer",
+                        used: false,
+                        onTeam: true,
+                        type: "h"
+                    }
+                },
             chosen = [],
             i,
             tLen = objCount(team),
-            rand;
+            rand,
+            pCount = 0;
 
-        function ifUsed(ranNum) {
-            var hr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 4, 5, 6, 1, 1, 1, 1],
-                hrLen = hr.length,
-                name,
-                newNum;
-            if (team[ranNum].used === false) {
-                name = team[ranNum].name;
-                if (rando(9, 1) === 1) {
-                    name += "s";
+                function ifUsed(ranNum) {
+                    var hr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 4, 5, 6, 1, 1, 1, 1];
+                    var hrLen = hr.length;
+                    var name;
+                    var newNum;
+                    if (team[ranNum].used === false && (team[ranNum].type === "h" || (team[ranNum].type === "p" && pCount < 1))) {
+                        name = team[ranNum].name;
+                        pCount = 1;
+                        if (rando(9, 1) === 1) {
+                            name += "s";
+                        }
+                        if (Math.floor(rando(13, 3) === 3)) {
+                            name += " hits " + hr[rando(hrLen, 1)] + " homeruns.";
+                        }
+                        chosen.push(name);
+                        team[ranNum].used = true;
+                    } else {
+                        newNum = rando(tLen, 1);
+                        ifUsed(newNum);
+                    }
                 }
-                if (Math.floor(rando(13, 3) === 3)) {
-                    name += " hits " + hr[rando(hrLen, 1)] + " homeruns.";
-                }
-                chosen.push(name);
-                team[ranNum].used = true;
-            } else {
-                newNum = rando(tLen, 1);
-                ifUsed(newNum);
-            }
-        }
         for (i = 0; i < 9; i++) {
             rand = rando(tLen, 1);
             ifUsed(rand);
